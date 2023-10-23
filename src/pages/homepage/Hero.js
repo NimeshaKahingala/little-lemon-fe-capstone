ba57898df0bf0f1d55e4ffdc298d6c7dc955401a
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2023-10-23 14:10:31 -0500

    hero section added

---
+++ src/pages/homepage/Hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import HeroImg from "../../assets/restauranfood.jpg";
+import './Hero.scss';
+
+function Hero() {
+    return <section className="section-hero">
+        <div className="container">
+        <div>
+            <div className="title">Little Lemon</div>
+            <div className="sub-title">Chicago</div>
+            <p className="text">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <Link className="btn-primary">Reserve a table</Link>
+        </div>
+        <div className="img-wrap">
+            <img src={HeroImg} className="hero-img"></img>
+        </div>
+        </div>
+    </section>
+}
+
+export default Hero;
