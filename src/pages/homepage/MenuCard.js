7fd03a3ab41474796c40bf0221a0897cd1f52731
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2023-10-23 14:10:58 -0500

    menu card added

---
+++ src/pages/homepage/MenuCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './MenuCard.scss';
+
+function MenuCard(props) {
+    return <article className="meal-card">
+        <img src={props.img} className="img-meal"></img>
+        <div className="meal-card-header">
+            <h3 className="name">{props.name}</h3>
+            <span className="price">{props.price}</span>
+        </div>
+        <div>
+            <p className="description">{props.description}</p>
+        </div>
+        <div className="delivery">Order a delivery <span class=" icon material-symbols-outlined">
+            motorcycle
+        </span></div>
+
+    </article>
+}
+
+export default MenuCard;
